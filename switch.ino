6730bcad4ecec7379a010c5141dec3c1e8273bcb
BINARYUPDATES.COM <binaryupdates@gmail.com>
2018-01-16 17:33:41 +0530

    Add files via upload

---
+++ switch.ino
@@ -0,0 +1,27 @@
+int ledPin = 13;
+int buttonPin = 7;
+
+int flag = 1;
+int buttonStatus = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+
+  int currentStatus = digitalRead(buttonPin);
+
+  if (currentStatus == HIGH && buttonStatus == LOW) {
+    if (flag == 0) {
+      digitalWrite(ledPin, HIGH);
+      flag = 1;
+    } else {
+      digitalWrite(ledPin, LOW);
+      flag = 0;
+    }
+  }
+
+  buttonStatus = currentStatus;
+}
